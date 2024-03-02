f68ac222e46fb16e256466cef69140f0ab6ddb2a
Hao Liu <11451150+haoliu-builder@users.noreply.github.com>
2024-03-02 12:49:56 -0700

    updated questionnaire

--- backend/data/create_db.sql
+++ backend/data/create_db.sql
@@ -32,6 +32,16 @@ CREATE TABLE IF NOT EXISTS "USER" (
     "password"    TEXT NOT NULL,
     PRIMARY KEY("id" AUTOINCREMENT)
 );
+DROP TABLE IF EXISTS "QUESTIONNAIRE";
+CREATE TABLE IF NOT EXISTS "QUESTIONNAIRE" (
+    "id"    INTEGER,
+    "mrn" INTEGER,
+    "q1"    TEXT,
+    "q2"    TEXT,
+    "q3"    TEXT,
+    PRIMARY KEY("id" AUTOINCREMENT)
+    FOREIGN KEY("mrn") REFERENCES PATIENT("mrn")
+);
 
 -- Inserting  fake data for testing
 INSERT INTO "USER" ("user_id", "password") VALUES ('Jon', '$2b$10$Q/UhyQB/nL46kxBAA10lyunFvClSdtj9MGPMcSncixlWRBU29YitK');
