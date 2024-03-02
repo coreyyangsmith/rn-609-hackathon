a4a4403904b126f72fac6205688c5c58ae499688
jsugimoto99 <jeremy.sugimoto@ucalgary.ca>
2024-03-02 11:41:57 -0700

    post request for reqistration.

--- my-app/app/(auth)/register.jsx
+++ my-app/app/(auth)/register.jsx
@@ -1,15 +1,45 @@
 import { Button, StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import { useAuth } from '@/context/auth';
 import LabelInputContainer from '../../components/LabelInputContainer';
+import axios from 'axios';
+
+
+
 
 
 export default function TabOneScreen() {
+    const [userData, setUserData] = useState({
+        MRN: '',
+        patient_name: '',
+        user_name: '',
+        user_ID: '',
+        password: '',
+        password_confirm:'',
+    });
+    
+    //user_password2 = '';
 
-    const { signIn } = useAuth();
+    const handleInputChange = (name, value) => {
+        setUserData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+    //const { signIn } = useAuth();
 
+    const register = () => {
+        print(userData)
+        const apiURL = 'http://localhost:3001/register';
+        axios.post(apiURL, userData)
+        .then(response => {
+            console.log("Account created Successfully", response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Create an Account</Text>
@@ -23,38 +53,44 @@ export default function TabOneScreen() {
                 text={"Patient's MRN"}
                 label={'Enter MRN'}
                 secure={false}
+                onChange={(value) => handleInputChange('MRN', value)}
             />
             <LabelInputContainer
                 text={"Patient Name"}
                 label={'Enter Name'}
                 secure={false}
+                onChange={(value) => handleInputChange('patient_name', value)}
             />
             <LabelInputContainer
                 text={"Your Name"}
                 label={'Enter Name'}
                 secure={false}
+                onChange={(value) => handleInputChange('user_name', value)}
             />
             <LabelInputContainer
                 text={"Create User ID"}
                 label={'Enter User ID'}
                 secure={false}
+                onChange={(value) => handleInputChange('user_ID', value)}
             />
             <LabelInputContainer
                 text={"Create Password"}
                 label={'Enter Password'}
                 secure={true}
+                onChange={(value) => handleInputChange('password', value)}
             />
             <LabelInputContainer
                 text={"Confirm Password"}
                 label={'Enter Password'}
                 secure={true}
+                onChange={(value) => handleInputChange('pasword_confirm', value)}
             />
             <View
                 style={styles.separator}
                 lightColor="#eee"
                 darkColor="rgba(255,255,255,0.1)"
             />
-            <Button title='Create Profile' color={'orange'} onPress={signIn} />
+            <Button title='Create Profile' color={'orange'} onPress={register} />
         </View>
     );
 }
