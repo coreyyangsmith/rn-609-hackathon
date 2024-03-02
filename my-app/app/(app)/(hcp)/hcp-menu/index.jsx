fd65c8a414f37bbae80e751874fbbc5f4ee72e4c
Jon Fulford <jonyfulford@gmail.com>
2024-03-02 12:23:54 -0700

    hcp menu

---
+++ my-app/app/(app)/(hcp)/hcp-menu/index.jsx
@@ -0,0 +1,46 @@
+import { View, Pressable, Text, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <StyledLink
+        text="Family Meeting Questionnaires"
+        route="hcp-questionnaire"
+      />
+      <StyledLink text="Family Meeting Recorder" route="hcp-recordings" />
+      <StyledLink text="Guided Meditations" route="hcp-meditations" />
+      <StyledLink text="Consult Spiritual Health" route="hcp-consultation" />
+      <StyledLink text="Resources for Professionals" route="hcp-resources" />
+    </View>
+  );
+}
+
+const StyledLink = ({ text, route }) => (
+  <Link href={route} asChild>
+    <Pressable style={styles.button}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </Pressable>
+  </Link>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    backgroundColor: "#007bff", // Example button color
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+    width: 250, // Adjust width as needed
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
--- my-app/app/(app)/index.jsx
+++ my-app/app/(app)/index.jsx
@@ -2,83 +2,90 @@ import { Pressable, Text } from "react-native";
 import { Link } from "expo-router";
 
 export default function Page() {
-    return (
-        <>
-            <Text>MAIN APP PAGE</Text>
-            <Text style={{ color: 'red' }}>Main Navigation</Text>
+  return (
+    <>
+      <Text>MAIN APP PAGE</Text>
+      <Text style={{ color: "red" }}>Main Navigation</Text>
 
-            <Link href="login" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Login</Text>
-                </Pressable>
-            </Link>
-            <Link href="register" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Register</Text>
-                </Pressable>
-            </Link>
+      <Link href="login" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Login</Text>
+        </Pressable>
+      </Link>
+      <Link href="register" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Register</Text>
+        </Pressable>
+      </Link>
 
+      <Text style={{ color: "red" }}>HCP Links</Text>
 
-            <Text style={{ color: 'red' }}>HCP Links</Text>
+      <Link href="hcp-menu" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Menu</Text>
+        </Pressable>
+      </Link>
 
-            <Link href="hcp-questionnaire" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Family Meeting Questionnaires</Text>
-                </Pressable>
-            </Link>
+      <Link href="hcp-questionnaire" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Family Meeting Questionnaires</Text>
+        </Pressable>
+      </Link>
 
-            <Link href="hcp-recordings" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Family Meeting Recorder</Text>
-                </Pressable>
-            </Link>
+      <Link href="hcp-recordings" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Family Meeting Recorder</Text>
+        </Pressable>
+      </Link>
 
-            <Link href="hcp-meditations" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Guided Meditations</Text>
-                </Pressable>
-            </Link>
+      <Link href="hcp-meditations" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Guided Meditations</Text>
+        </Pressable>
+      </Link>
 
-            <Link href="hcp-consultation" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Consult Spiritual Health</Text>
-                </Pressable>
-            </Link>
+      <Link href="hcp-consultation" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Consult Spiritual Health</Text>
+        </Pressable>
+      </Link>
 
-            <Link href="hcp-resources" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Resources for Professionals</Text>
-                </Pressable>
-            </Link>
+      <Link href="hcp-resources" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Resources for Professionals</Text>
+        </Pressable>
+      </Link>
 
+      {/* Patient Links */}
+      <Text style={{ color: "red" }}>Patient Links</Text>
 
-            {/* Patient Links */}
-            <Text style={{ color: 'red' }}>Patient Links</Text>
+      <Link href="pat-questionnaire" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>
+            Pre-Family Meeting Questionnaire
+          </Text>
+        </Pressable>
+      </Link>
 
-            <Link href="pat-questionnaire" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Pre-Family Meeting Questionnaire</Text>
-                </Pressable>
-            </Link>
+      <Link href="pat-recordings" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>
+            Previous Family Meeting Recordings
+          </Text>
+        </Pressable>
+      </Link>
 
-            <Link href="pat-recordings" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Previous Family Meeting Recordings</Text>
-                </Pressable>
-            </Link>
+      <Link href="pat-meditations" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Guided Meditations</Text>
+        </Pressable>
+      </Link>
 
-            <Link href="pat-meditations" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Guided Meditations</Text>
-                </Pressable>
-            </Link>
-
-            <Link href="pat-resources" asChild>
-                <Pressable>
-                    <Text style={{ color: 'blue' }}>Resources for Practical Matters</Text>
-                </Pressable>
-            </Link>
-
-        </>
-    );
+      <Link href="pat-resources" asChild>
+        <Pressable>
+          <Text style={{ color: "blue" }}>Resources for Practical Matters</Text>
+        </Pressable>
+      </Link>
+    </>
+  );
 }
