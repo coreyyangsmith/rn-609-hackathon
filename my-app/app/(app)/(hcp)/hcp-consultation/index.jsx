578186ad29108b6160e44e379ee1622163a46248
ESDiep <eric.diepvn@gmail.com>
2024-03-02 14:18:41 -0700

    create consult spiritual health page

--- my-app/app/(app)/(hcp)/hcp-consultation/index.jsx
+++ my-app/app/(app)/(hcp)/hcp-consultation/index.jsx
@@ -1,48 +1,115 @@
+import LabelInputContainer from '../../../../components/LabelInputContainer'
 
-import { Button, StyleSheet } from 'react-native';
+
+import { Button, StyleSheet, TextInput } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import { useAuth } from '@/context/auth';
-import LabelInputContainer from '../../../../components/LabelInputContainer'
 import axios from 'axios';
 import { useState } from 'react';
 
-export default function Page() {
+export default function TabOneScreen() {
+    const [userData, setUserData] = useState({
+        MRN: '',
+        patient_name: '',
+        user_name: '',
+        user_ID: '',
+        password: '',
+        password_confirm: '',
+    });
+
+    //user_password2 = '';
+
+    const handleInputChange = (name, value) => {
+        setUserData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+    //const { signIn } = useAuth();
+
+    const send = () => {
+        print(userData)
+        const apiURL = 'http://localhost:3001/register';
+        axios.post(apiURL, userData)
+            .then(response => {
+                console.log("Account created Successfully", response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
     return (
         <View style={styles.container}>
-        <Text style={styles.title}>Consult Spi Health</Text>
-        <View
-            style={styles.separator}
-            lightColor="#eee"
-            darkColor="rgba(255,255,255,0.1)"
-        />
+            <Text style={styles.title}>Consult Spiritual Health</Text>
+            <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+            />
 
-        <LabelInputContainer
-            text={"Patient's MRN"}
-            label={'Enter MRN'}
-            secure={false}
-            onChange={(value) => handleInputChange('MRN', value)}
-        />
-        <LabelInputContainer
-            text={"Patient Name"}
-            label={'Enter Name'}
-            secure={false}
-            onChange={(value) => handleInputChange('patient_name', value)}
-        />
-        <LabelInputContainer
-            text={"Unit"}
-            label={'Unit'}
-            secure={false}
-            onChange={(value) => handleInputChange('user_name', value)}
-        />
+            <LabelInputContainer
+                text={"Patient's MRN"}
+                label={'Enter MRN'}
+                secure={false}
+                onChange={(value) => handleInputChange('MRN', value)}
+            />
+            <LabelInputContainer
+                text={"Patient Name"}
+                label={'Enter Name'}
+                secure={false}
+                onChange={(value) => handleInputChange('patient_name', value)}
+            />
+            <LabelInputContainer
+                text={"Unit"}
+                label={'Enter Unit'}
+                secure={false}
+                onChange={(value) => handleInputChange('unit', value)}
+            />
+            
+            <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+            />
+
+        <Text style={styles.title}>Brief description</Text>
+            <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+            />
+
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}       
+        placeholder="Enter text here"
         
-        <View
-            style={styles.separator}
-            lightColor="#eee"
-            darkColor="rgba(255,255,255,0.1)"
-        />
-        <Button title='Create Profile' color={'orange'} onPress={register} />
-    </View>
+      />
+        </View>
+
+            <Button title='Send Consult' color={'orange'} onPress={send} />
+        </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+    input: {
+        width: 400,
+        height:60,
+    },
+});
