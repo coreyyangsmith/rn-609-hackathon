e6ada2b001d0b331d29efa063fdaa70b5bb6ce32
Israel Suarez Robles <oak.isr@gmail.com>
2024-03-02 13:54:49 -0700

    added phone back

--- backend/src/controllers/authController.mjs
+++ backend/src/controllers/authController.mjs
@@ -9,13 +9,13 @@ const saltRounds = 10;
 
 export const register = withDatabase(
     asyncHandler(async (req, res, next) => {
-        const { mrn,patient_name,user_name,user_id, password } = req.body;
+        const {mrn, patient_name, user_name, user_id, password} = req.body;
 
         // Query database for user with the given user_id
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
         // Attempt to insert new user into database
-        const response = await req.db.execute('INSERT INTO USER (mrn,patient_name,user_name,user_id, password) VALUES (?, ?,?,?,?)', [mrn,patient_name,user_name,user_id, hashedPassword]);
+        const response = await req.db.execute('INSERT INTO USER (mrn,patient_name,user_name,user_id, password) VALUES (?, ?,?,?,?)', [mrn, patient_name, user_name, user_id, hashedPassword]);
         console.log(response);
 
         res.json({
@@ -31,19 +31,19 @@ export const register = withDatabase(
 // }
 export const login = withDatabase(
     asyncHandler(async (req, res, next) => {
-        const { user_id, password } = req.body;
+        const {user_id, password} = req.body;
         console.log(user_id, password);
 
         // Query database for user with the given user_id
         const searchResult = await req.db.queryOne('SELECT * FROM USER WHERE user_id = ?', [user_id]);
-        if(!searchResult) {
-            return res.status(404).send("User not found" );
+        if (!searchResult) {
+            return res.status(404).send("User not found");
         }
         console.log(searchResult.phone);
 
         // Compare hashed password from database with input password
         const isPasswordCorrect = await bcrypt.compare(password, searchResult.password);
-        if(isPasswordCorrect) {
+        if (isPasswordCorrect) {
             await OTP.sendSMS(searchResult.phone, user_id)
             res.send("Password is correct");
         } else {
@@ -55,9 +55,9 @@ export const login = withDatabase(
 
 export const verifyCode = withDatabase(
     asyncHandler(async (req, res, next) => {
-        const { user_id, code } = req.body;
+        const {user_id, code} = req.body;
 
-        if(OTP.verifyCode(user_id, code)){
+        if (OTP.verifyCode(user_id, code)) {
             res.json({
                 message: "success"
             });
